7684d7e26d08d16794c4be1cfbf033cf8cbe6a4f
milos-hynek <134530320+milos-hynek@users.noreply.github.com>
2024-04-14 14:29:53 +0200

    Aktualizace routes

--- air-quality-b.tes-t.cz/engine/systemovaUrl.php
+++ air-quality-b.tes-t.cz/engine/systemovaUrl.php
@@ -5,6 +5,7 @@ $systemoveUrl["/?modul=aUzivatele"]="administrace/uzivatele/";
 $systemoveUrl["/?modul=aSmazaniUzivatele"]="administrace/uzivatele/smazani-uzivatele/";
 $systemoveUrl["/?modul=aNastaveni"]="administrace/nastaveni/";
 $systemoveUrl["/?modul=aPromenneNastaveni"]="administrace/nastaveni/promenne-nastaveni/";
+$systemoveUrl["/?modul=aNamerenaData"]="administrace/namerena-data/";
 $systemoveUrl["/?modul=Uvod"]="uvod/";
 $systemoveUrl["/?modul=Sitemapa"]="404/";
 $systemoveUrl["/?modul=Sitemapa&akce=robots"]="robots.txt";
