64aea68a660278e546774cd7c43038d6824e1dd8
milos-hynek <134530320+milos-hynek@users.noreply.github.com>
2024-05-06 16:24:36 +0200

    Delete air-quality-b.tes-t.cz/url directory

--- air-quality-b.tes-t.cz/url/urlaNamerenaData.php
+++
@@ -1,4 +0,0 @@
-<?php
-global $systemoveUrl;
-$systemoveUrl["/?modul=aNamerenaData&akce=vypis-dat"]="administrace/namerena-data/hp/";
-$systemoveUrl["/?modul=aNamerenaData&akce=zmena-vyhledavani"]="administrace/namerena-data/zmenit-vyhledavani/";
--- air-quality-b.tes-t.cz/url/urlaNastaveni.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $systemoveUrl;
-$systemoveUrl["/?modul=aNastaveni&akce=vypis-nastaveni"]="administrace/nastaveni/hp/";
-$systemoveUrl["/?modul=aNastaveni&akce=vypis-nastaveni&m=nastaveni-nenalezeno"]="administrace/nastaveni/hp/nastaveni-nenalezeno/";
-$systemoveUrl["/?modul=aNastaveni&akce=editace-nastaveni"]="administrace/nastaveni/editace/";
-$systemoveUrl["/?modul=aNastaveni&akce=editace-nastaveni&m=nastaveni-ulozeno"]="administrace/nastaveni/editace/nastaveni-ulozeno/";
-$systemoveUrl["/?modul=aNastaveni&akce=uloz-nastaveni"]="administrace/nastaveni/ulozit-nastaveni/";
-$systemoveUrl["/?modul=aNastaveni&akce=zmena-vyhledavani"]="administrace/nastaveni/zmenit-vyhledavani/";
-$systemoveUrl["/?modul=aNastaveni&akce=uloz-soubor-promenne"]="administrace/nastaveni/ulozit-soubor/";
