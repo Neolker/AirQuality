0323a0462d9a031d9cc9071d9b27bdc16664f387
milos-hynek <134530320+milos-hynek@users.noreply.github.com>
2024-05-06 16:23:34 +0200

    Aktualizace URL projektu

---
+++ air-quality-b.tes-t.cz/url/urlaNamerenaData.php
@@ -0,0 +1,4 @@
+<?php
+global $systemoveUrl;
+$systemoveUrl["/?modul=aNamerenaData&akce=vypis-dat"]="administrace/namerena-data/hp/";
+$systemoveUrl["/?modul=aNamerenaData&akce=zmena-vyhledavani"]="administrace/namerena-data/zmenit-vyhledavani/";
