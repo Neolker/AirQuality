aa402816b49bc7c0e2538b170f142c39f75d1cd9
marcushovi <hovimarcus@gmail.com>
2024-05-29 21:22:04 +0200

    chore: Refactor AuthenticationWrapper component
    Refactor the AuthenticationWrapper component to use the useUser hook from the UserContext. This simplifies the code and improves code readability.

--- frontend/src/components/AuthenticationWrapper.tsx
+++ frontend/src/components/AuthenticationWrapper.tsx
@@ -1,35 +1,39 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { Box, LoadingOverlay } from "@mantine/core";
 import { useRouter } from "next/navigation";
-import { parseCookies } from "nookies";
-import { Loader } from "@mantine/core";
+import { useUser } from "./Contexts/UserContext";
 
 const AuthenticationWrapper = ({ children }: { children: React.ReactNode }) => {
-  const [isLoading, setIsLoading] = useState(true);
+  const { isLoading, login, user } = useUser();
   const router = useRouter();
 
-  useEffect(() => {
-    const checkAuth = async () => {
-      const cookies = parseCookies();
-      const session = cookies.user ? JSON.parse(cookies.user)?.session : null;
+  // if (isLoading) {
+  //   return (
+  //     <Center>
+  //       <Container mt="xl">
+  //         <Loader color="blue" />
+  //       </Container>
+  //     </Center>
+  //   ); // Show loader while checking authentication
+  // }
 
-      if (!session) {
-        router.push("/");
-        // window.location.href = "/"
-      } else {
-        setIsLoading(false); // Set loading to false if authenticated
-      }
-    };
-
-    checkAuth();
-  }, [router]);
-
-  if (isLoading) {
-    return <Loader color="blue" />; // Show loader while checking authentication
+  if (!isLoading && !user) {
+    router.push("/"); // Redirect to login page if not authenticated
+    return null;
   }
 
-  return <>{children}</>; // Render children if authenticated
+  return (
+    <Box pos="relative">
+      <LoadingOverlay
+        visible={isLoading}
+        zIndex={1000}
+        overlayProps={{ radius: "md", blur: 4 }}
+        loaderProps={{ size: "xl", type: "dots" }}
+      />
+      {children}
+    </Box>
+  ); // Render children if authenticated
 };
 
 export default AuthenticationWrapper;
