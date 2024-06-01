ead9559c59292cd48212ae50419947e595f95e47
Adam Mácha <adammacha@Adam--MacBook-Air.local>
2024-06-01 19:37:48 +0200

    dashboard update

--- frontend/src/components/app/tables/DeviceTable.tsx
+++ frontend/src/components/app/tables/DeviceTable.tsx
@@ -2,107 +2,18 @@
 
 import { Table, Progress, Anchor, Text, Group } from '@mantine/core';
 import classes from "./DeviceTable.module.css"
-
-const data = [
-  {
-    title: 'Foundation',
-    author: 'Isaac Asimov',
-    year: 1951,
-    reviews: { positive: 2223, negative: 259 },
-  },
-  {
-    title: 'Frankenstein',
-    author: 'Mary Shelley',
-    year: 1818,
-    reviews: { positive: 5677, negative: 1265 },
-  },
-  {
-    title: 'Solaris',
-    author: 'Stanislaw Lem',
-    year: 1961,
-    reviews: { positive: 3487, negative: 1845 },
-  },
-  {
-    title: 'Dune',
-    author: 'Frank Herbert',
-    year: 1965,
-    reviews: { positive: 8576, negative: 663 },
-  },
-  {
-    title: 'The Left Hand of Darkness',
-    author: 'Ursula K. Le Guin',
-    year: 1969,
-    reviews: { positive: 6631, negative: 993 },
-  },
-  {
-    title: 'A Scanner Darkly',
-    author: 'Philip K Dick',
-    year: 1977,
-    reviews: { positive: 8124, negative: 1847 },
-  },
-];
-
-const devices_mock = [
-    {
-      device_id: 1,
-      serial_number: "SN-001",
-      name: "Device 1",
-      status: "Online",
-      location: "Location 1",
-      co2_green: 400,
-      co2_yellow: 800,
-      co2_red: 1200,
-      air_quality: 30
-    },
-    {
-      device_id: 2,
-      serial_number: "SN-002",
-      name: "Device 2",
-      status: "Offline",
-      location: "Location 2",
-      co2_green: 400,
-      co2_yellow: 800,
-      co2_red: 1200,
-      air_quality: 30
-    },
-    {
-      device_id: 3,
-      serial_number: "SN-003",
-      name: "Device 3",
-      status: "Online",
-      location: "Location 3",
-      co2_green: 400,
-      co2_yellow: 800,
-      co2_red: 1200,
-      air_quality: 30
-    },
-    {
-      device_id: 4,
-      serial_number: "SN-004",
-      name: "Device 4",
-      status: "Offline",
-      location: "Location 4",
-      co2_green: 400,
-      co2_yellow: 800,
-      co2_red: 1200,
-      air_quality: 30
-    },
-    {
-      device_id: 5,
-      serial_number: "SN-005",
-      name: "Device 5",
-      status: "Online",
-      location: "Location 5",
-      co2_green: 400,
-      co2_yellow: 800,
-      co2_red: 1200,
-      air_quality: 30
-    },
-  ];
+import { useDevices } from '@/components/Contexts/DeviceContext';
 
 export function DeviceTable() {
-  const rows = devices_mock.map((row) => {
-    const qualityIndex = row.air_quality
+
+  const { devices } = useDevices();
+  const onlineDevicess = devices.filter((devicee) => devicee.status === "Online");
+
+  const onlineDevices = devices.filter((device) => device.status === "Online");
+
+
+  const rows = devices.map((row) => {
+    const qualityIndex = 33 //TODO
     // const positiveReviews = (row.reviews.positive / totalReviews) * 100;
     // const negativeReviews = (row.reviews.negative / totalReviews) * 100;
 
@@ -140,6 +51,12 @@ export function DeviceTable() {
               className={classes.progressSection}
               value={qualityIndex}
               color="red"
+            />
+
+            <Progress.Section
+              className={classes.progressSection}
+              value={qualityIndex}
+              color="yellow"
             />
           </Progress.Root>
         </Table.Td>
