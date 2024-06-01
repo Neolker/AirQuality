0a321553721e2b0ba803b2e8d6b6b35b9e2df5ff
marcushovi <hovimarcus@gmail.com>
2024-06-01 12:22:53 +0200

    feat: Add Skeleton loading for DeviceCard component

--- frontend/src/components/app/DeviceCard.tsx
+++ frontend/src/components/app/DeviceCard.tsx
@@ -4,6 +4,7 @@ import {
   Card,
   Divider,
   Group,
+  Skeleton,
   Stack,
   Text,
   useMantineTheme,
@@ -28,6 +29,8 @@ interface DeviceCardProps {
   co2_yellow: number;
   co2_red: number;
   onEdit: ({ device }: any) => void;
+  onDelete: ({ device }: any) => void;
+  isLoading: boolean;
 }
 
 export const DeviceCard = ({
@@ -40,79 +43,90 @@ export const DeviceCard = ({
   co2_yellow,
   co2_red,
   onEdit,
+  onDelete,
+  isLoading,
 }: DeviceCardProps) => {
   const theme = useMantineTheme();
   const router = useRouter();
   return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Group justify="space-between" grow style={{ marginBottom: 5 }}>
-        <Text w={500}>{name}</Text>
-        <Group justify="flex-end">
-          <Badge color={status === "Online" ? "green" : "red"}>{status}</Badge>
+    <Skeleton visible={isLoading} radius="md">
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Group justify="space-between" grow style={{ marginBottom: 5 }}>
+          <Text w={500}>{name}</Text>
+          <Group justify="flex-end">
+            <Badge color={status === "Online" ? "green" : "red"}>
+              {status}
+            </Badge>
+          </Group>
         </Group>
-      </Group>
-      <Stack>
-        <Group>
-          <IconCpu2 size={16} />
-          <Text size="sm">Serial Number: {serial_number}</Text>
-        </Group>
-        <Group>
-          <IconMapPin size={16} />
-          <Text size="sm">Location: {location}</Text>
-        </Group>
-        <Divider my="xs" label="CO2 Levels" labelPosition="center" />
-        <Group>
-          <IconPlayerRecordFilled size={16} color="green" />
-          <Text size="sm">Green Level: {co2_green} ppm</Text>
-        </Group>
-        <Group>
-          <IconPlayerRecordFilled size={16} color="orange" />
-          <Text size="sm">Yellow Level: {co2_yellow} ppm</Text>
-        </Group>
-        <Group>
-          <IconPlayerRecordFilled size={16} color="red" />
-          <Text size="sm">Red Level: {co2_red} ppm</Text>
-        </Group>
-      </Stack>
-      <Card.Section mt="md" py="xs" px="lg" withBorder>
-        <Group justify="space-between">
+        <Stack>
           <Group>
-            <ActionIcon
-              variant="subtle"
-              size="lg"
-              onClick={() => {
-                router.push(`/app/devices/${device_id}`);
-              }}
-            >
-              <IconChartLine
-                style={{ width: "70%", height: "70%" }}
-                color={theme.colors.blue[6]}
-                stroke={1.5}
-              />
-            </ActionIcon>
+            <IconCpu2 size={16} />
+            <Text size="sm">SN: {serial_number}</Text>
+          </Group>
+          <Group>
+            <IconMapPin size={16} />
+            <Text size="sm">Location: {location}</Text>
+          </Group>
+          <Divider my="xs" label="CO2 Levels" labelPosition="center" />
+          <Group>
+            <IconPlayerRecordFilled size={16} color="green" />
+            <Text size="sm">Green Level: {co2_green} ppm</Text>
+          </Group>
+          <Group>
+            <IconPlayerRecordFilled size={16} color="orange" />
+            <Text size="sm">Yellow Level: {co2_yellow} ppm</Text>
+          </Group>
+          <Group>
+            <IconPlayerRecordFilled size={16} color="red" />
+            <Text size="sm">Red Level: {co2_red} ppm</Text>
+          </Group>
+        </Stack>
+        <Card.Section mt="md" py="xs" px="lg" withBorder>
+          <Group justify="space-between">
+            <Group>
+              <ActionIcon
+                variant="subtle"
+                size="lg"
+                onClick={() => {
+                  router.push(`/app/devices/${device_id}`);
+                }}
+              >
+                <IconChartLine
+                  style={{ width: "70%", height: "70%" }}
+                  color={theme.colors.blue[6]}
+                  stroke={1.5}
+                />
+              </ActionIcon>
+              <ActionIcon
+                variant="subtle"
+                color="gray"
+                size="lg"
+                onClick={onEdit}
+              >
+                <IconEdit
+                  style={{ width: "70%", height: "70%" }}
+                  color={theme.colors.blue[6]}
+                  stroke={1.5}
+                />
+              </ActionIcon>
+            </Group>
+
             <ActionIcon
               variant="subtle"
               color="gray"
               size="lg"
-              onClick={onEdit}
+              onClick={onDelete}
             >
-              <IconEdit
+              <IconUnlink
                 style={{ width: "70%", height: "70%" }}
-                color={theme.colors.blue[6]}
+                color={theme.colors.red[6]}
                 stroke={1.5}
               />
             </ActionIcon>
           </Group>
-
-          <ActionIcon variant="subtle" color="gray" size="lg">
-            <IconUnlink
-              style={{ width: "70%", height: "70%" }}
-              color={theme.colors.red[6]}
-              stroke={1.5}
-            />
-          </ActionIcon>
-        </Group>
-      </Card.Section>
-    </Card>
+        </Card.Section>
+      </Card>
+    </Skeleton>
   );
 };
